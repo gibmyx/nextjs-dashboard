7191f1b6cb6d74b5bc392dee3c5e1a7bdb957df2
Gibmyx <gibmyxjobs@gmail.com>
2025-01-31 14:29:06 -0400

    conextion DB

--- app/query/route.ts
+++ app/query/route.ts
@@ -13,17 +13,17 @@
 // 	return data.rows;
 // }
 
-// export async function GET() {
-//   return Response.json({
-//     message:
-//       'Uncomment this file and remove this line. You can delete this file when you are finished.',
-//   });
+export async function GET() {
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
   // try {
   // 	return Response.json(await listInvoices());
   // } catch (error) {
   // 	return Response.json({ error }, { status: 500 });
   // }
-// }
+}
 //
 // import { createClient } from '@supabase/supabase-js';
 //
